058cb91ecd7de6e823e84c2137916d8007937239
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-21 20:37:19 +0300

    add little timer

--- react-ts-hooks/src/components/UseEffectComponent.tsx
+++ react-ts-hooks/src/components/UseEffectComponent.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
-const UseEffectComponent = (props: Props) => {
+const UseEffectComponent = () => {
+    const [value, setValue] = useState(1)
+    useEffect(() => {
+        const intervalId = window.setInterval(() => setValue(prev => prev + 1), 1000)
+        return () => window.clearInterval(intervalId)
+    }, [])
+
     return (
-        <div>UseEffectComponent</div>
+        <>
+            <div>{value}</div>
+        </>
     )
 }
 
