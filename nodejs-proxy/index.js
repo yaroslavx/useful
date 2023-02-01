4c08986676aefabecf2422c5d8794a944116ec16
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-02-01 15:28:29 +0300

    setup node server

---
+++ nodejs-proxy/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+app.use('/api', require('./routes'));
+
+app.listen(PORT, () => {
+  console.log(`Server runnig on ${PORT}`);
+});
