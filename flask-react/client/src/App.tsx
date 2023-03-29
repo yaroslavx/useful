eadb3adc33e869922da2f02d17bb27ea61a481ff
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-03-29 23:05:25 +0300

    Add sop on server and add testing client

--- flask-react/client/src/App.tsx
+++ flask-react/client/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [members, setMembers] = useState([])
+  useEffect(() => {
+    const fetchMembers = async () => {
+      const res = await fetch('http://localhost:5000/members')
+      const data = await res.json()
+      setMembers(data.members)
+    }
+
+    fetchMembers()
+  }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {members.map((member) => <div key={member}>{member}</div>)}
     </div>
   )
 }
--- flask-react/server/server.py
+++ flask-react/server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/members/*": {"origins": "http://localhost:5173"}})
 
 #  Members API Route
 @app.route('/members')
