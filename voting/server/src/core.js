ac67954ab75d251d10e887de25109f48b0610094
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-05-07 11:34:56 +0300

    Modify tests and add some logic

--- voting/server/src/core.js
+++ voting/server/src/core.js
@@ -1,5 +1,17 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
-export function setEntries(state, entries) {
+export const setEntries = (state, entries) => {
   return state.set('entries', List(entries));
-}
+};
+
+export const next = (state) => {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({ pair: entries.take(2) }),
+    entries: entries.skip(2),
+  });
+};
+
+export const vote = (state, entry) => {
+  return state.updateIn(['vote', 'tally', entry], 0, (tally) => tally + 1);
+};
--- voting/server/test/core_spec.js
+++ voting/server/test/core_spec.js
@@ -27,4 +27,71 @@ describe('application logic', () => {
       );
     });
   });
+
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine'),
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+          }),
+          entries: List.of('Sunshine'),
+        })
+      );
+    });
+  });
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+        }),
+        entries: List(),
+      });
+
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              Trainspotting: 1,
+            }),
+          }),
+          entries: List(),
+        })
+      );
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            Trainspotting: 3,
+            '28 Days Later': 2,
+          }),
+        }),
+        entries: List(),
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              Trainspotting: 4,
+              '28 Days Later': 2,
+            }),
+          }),
+          entries: List(),
+        })
+      );
+    });
+  });
 });
