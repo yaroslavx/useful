07dde075a423d63cf93ba9a569b605afe6f5c29c
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-11-19 16:03:11 +0300

    svelte-canvas: add mouse interactivity

--- svelte-canvas/src/App.svelte
+++ svelte-canvas/src/App.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
     import Canvas from "./lib/Canvas.svelte";
     import Line from "./lib/Line.svelte";
+  import type { TPoint } from "./types";
+
+    function followMouse(e: any) {
+        let rect = e.target.getBoundingClientRect()
+        end = [e.clientX - rect.left, e.clientY - rect.top]
+    }
+
+    let start = [0, 0] as TPoint
+    let end = [0, 0] as TPoint
 </script>
 
 <main>
-    <Canvas>
-        <Line start={[10, 20]} end={[100, 200]}/>
-        <Line start={[20, 30]} end={[200, 300]}/>
+    <Canvas on:mousemove={followMouse} on:mouseleave={() => {
+        end = [0, 0]
+    }}>
+        <Line {start} {end}/>
     </Canvas>
 </main> 
--- svelte-canvas/src/lib/Canvas.svelte
+++ svelte-canvas/src/lib/Canvas.svelte
@@ -1,9 +1,12 @@
 <script lang='ts'>
-    import { onMount, setContext } from "svelte";
+    import { onDestroy, onMount, setContext } from "svelte";
     import type { TDrawFn} from '../types'
+
+    export let clearFrames: boolean = true
 
     let canvasElement: HTMLCanvasElement
     let fnsToDraw = [] as TDrawFn[]
+    let frameId: number
 
     setContext("canvas", {
         addDrawFn: (fn: TDrawFn) => {
@@ -20,13 +23,24 @@
 
     onMount(() => {
         let ctx = canvasElement.getContext('2d')
-        ctx && draw(ctx)
+        frameId = requestAnimationFrame(() => ctx && draw(ctx))
+    })
+
+    onDestroy(() => {
+        if (frameId) {
+            cancelAnimationFrame(frameId)
+        }
     })
 
     function draw(ctx: CanvasRenderingContext2D) {
+        if (clearFrames) {
+            ctx.clearRect(0, 0, canvasElement.width, canvasElement.width)
+        }
+
         fnsToDraw.forEach(fn => fn(ctx))
+        frameId = requestAnimationFrame(() => draw(ctx))
     }
 </script>
 
-<canvas bind:this={canvasElement}/>
+<canvas on:mousemove on:mouseleave bind:this={canvasElement}/>
 <slot/>
